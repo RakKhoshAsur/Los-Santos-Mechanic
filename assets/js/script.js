69b68e67661bb143b5d01a68c70275ab8aafca15
Shanu Dey <sd4shanudey@gmail.com>
2021-03-13 15:11:54 +0530

    Add upgrades list add MEOS field generate mechanic log autometically
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,27 @@ function validateInput(){
     console.log("Price with Commision ="+priceWithCommision);
     ns.value = priceWithCommision;
     
-    //set toal
+    //set total
     let total = parseInt(document.getElementById("total").value);
     total += priceWithCommision;
     document.getElementById("total").value = total;
+
+    //add upgrades
+    let upgrades = document.getElementById("upgrades").value;
+    upgrades+=event.target.name+", ";
+    document.getElementById("upgrades").value = upgrades;
+}
+
+function submitMEOS(){
+    let meos = document.getElementById("meosString").value;
+    console.log(meos);
+    let arr = meos.split("\n");
+    console.log(arr);
+    var result ="Name =" + arr[2].split(":")[1] +"\n";
+    result += "Gadi no. =" + arr[0].split(":")[1]+"\n";
+    result += "Upgrades =" + document.getElementById("upgrades").value+"\n";
+    result += "Total Costing=" + document.getElementById("total").value+"\n";
+    result += "My Cut ="+ parseInt(document.getElementById("total").value)*100/(100+parseInt(document.getElementById("commission").value));
+    console.log(result);
+    document.getElementById("mechanicLog").value = result;
 }
