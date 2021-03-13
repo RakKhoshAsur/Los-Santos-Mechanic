609566d1be38050efd06525f591ceae912e08c76
Shanu Dey <sd4shanudey@gmail.com>
2021-03-13 15:19:08 +0530

    added copy to clipboard option
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,18 @@ function submitMEOS(){
     console.log(result);
     document.getElementById("mechanicLog").value = result;
 }
+
+function copyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("mechanicLog");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Mechanic Log copied to clipboard");
+  }
