7f44dee3a9e164d809d565817bf6d341de3cde72
Shanu Dey <sd4shanudey@gmail.com>
2021-03-13 14:30:11 +0530

    added total field default commision 25% calculate price with commission add this price with total price
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 function validateInput(){
-    //console.log(event.target.name);
-    var element = document.getElementsByName(event.target.tagName);
+    // console.log(event.target.name);
+    let price= parseInt(event.target.value);
+    event.target.value = price;
+
+    //set price with commission
+    let ns = event.target.nextSibling.nextSibling;
+    let commission = parseInt(document.getElementById("commission").value);
+    let priceWithCommision = parseInt(price)+(price * commission / 100);
+    console.log("Price with Commision ="+priceWithCommision);
+    ns.value = priceWithCommision;
+    
+    //set toal
+    let total = parseInt(document.getElementById("total").value);
+    total += priceWithCommision;
+    document.getElementById("total").value = total;
 }
