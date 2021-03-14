05683e0dbe5137dd9fba09c96793d3762bb49904
Shanu Dey <sd4shanudey@gmail.com>
2021-03-14 10:45:48 +0530

    modified logic to compensate total price, if any extra modification added console logs disabled
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
-function validateInput(){
+function validateInput(input){
+    // console.log("new value = "+input.value);
+    // console.log("old value = "+input.oldvalue);
+
     // console.log(event.target.name);
-    let price= parseInt(event.target.value);
-    event.target.value = price;
+    let price= parseInt(input.value);
+    input.value = price;
+    let oldprice = parseInt(input.oldvalue);
 
     //set price with commission
     let ns = event.target.nextSibling.nextSibling;
     let commission = parseInt(document.getElementById("commission").value);
-    let priceWithCommision = parseInt(price)+(price * commission / 100);
-    console.log("Price with Commision ="+priceWithCommision);
+    let priceWithCommision = price+(price * commission / 100);
+    //console.log("Price with Commision ="+priceWithCommision);
     ns.value = priceWithCommision;
     
     //set total
     let total = parseInt(document.getElementById("total").value);
     total += priceWithCommision;
+    total -= (oldprice+(oldprice*commission/100));
     document.getElementById("total").value = total;
 
     //add upgrades
@@ -23,20 +28,23 @@ function validateInput(){
 
 function submitMEOS(){
     let meos = document.getElementById("meosString").value;
-    console.log(meos);
+    //console.log(meos);
     let arr = meos.split("\n");
-    console.log(arr);
+    //console.log(arr);
     let total = parseInt(document.getElementById("total").value);
     let commission = parseInt(document.getElementById("commission").value);
     let profit = (total*commission)/(100+commission);
-
+    let upgradeSet = new Set(document.getElementById("upgrades").value.split(", "));
+    let upgrades = Array.from(upgradeSet).join(", ");
+    //console.log(upgrades);
+    
     // generate mechanic log
     var result ="Name =" + arr[2].split(":")[1] +"\n";
     result += "Gadi no. =" + arr[0].split(":")[1]+"\n";
-    result += "Upgrades =" + document.getElementById("upgrades").value+"\n";
+    result += "Upgrades =" + upgrades+"\n";
     result += "Total Costing=" + total+"\n";
     result += "My Cut ="+ profit;
-    console.log(result);
+    //console.log(result);
     document.getElementById("mechanicLog").value = result;
 }
 
